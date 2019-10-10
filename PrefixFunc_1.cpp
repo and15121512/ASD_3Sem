91440df16d8d966622ae79250840f1b7ae402e93
and15121512 <andrew15121512@yandex.ru>
2019-10-10 16:18:25 +0300

    mast commit

--- PrefixFunc_1.cpp
+++ PrefixFunc_1.cpp
@@ -8,7 +8,7 @@ uint32_t PrefixFunc(const std::vector<uint32_t>& prefix_funcs, const std::vector
 	uint32_t prev_prefix_func, char curr_symbol) {
 	if (prev_prefix_func == UINT32_MAX)
 		return 0;
-	
+
 	uint32_t j = prev_prefix_func;
 	while (j > 0 && curr_symbol != pattern[j])
 		j = prefix_funcs[j - 1];
@@ -21,18 +21,18 @@ uint32_t PrefixFunc(const std::vector<uint32_t>& prefix_funcs, const std::vector
 
 std::vector<uint32_t> AllStringOccurences(const std::vector<char>& pattern) {
 	const char str_delimiter = '#';
-	
+
 	std::vector<char> pattern_with_delimiter = pattern;
 	pattern_with_delimiter.push_back(str_delimiter);
 
 	std::vector<uint32_t> occurences;
 	std::vector<uint32_t> prefix_funcs(pattern_with_delimiter.size(), 0); // храним только для паттерна, который ищем
 	uint32_t prev_prefix_func = UINT32_MAX;
-	
+
 	uint32_t index = 0;
 	for (const char& symbol : pattern_with_delimiter) {
 		uint32_t curr_prefix_func = PrefixFunc(prefix_funcs, pattern_with_delimiter, prev_prefix_func, symbol);
-		
+
 		prev_prefix_func = curr_prefix_func;
 		prefix_funcs[index] = curr_prefix_func;
 
