ca20717adfe33233e28b994cf3a1d97ab00ecb59
and15121512 <andrew15121512@yandex.ru>
2019-11-24 11:48:58 +0300

    fixed task2 1module

--- MinCorrespString_2.cpp
+++ MinCorrespString_2.cpp
@@ -1,58 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <bitset>
 
 
 
+constexpr uint32_t alphabet_size = 26;
 // This function considers case when we need to run through all prefixes to
 // check that there is no prefixes that will be continued by adding new symbol to our result line
-void FindMinStringCasePrefixFuncIsNull(const std::vector<uint32_t>& prefix_funcs
-									, std::vector<uint32_t>& result_arr
-									, uint32_t index) {
-	const uint32_t alphabet_size = 26;
-
-	std::vector<bool> does_character_continue_string(alphabet_size, false);
-
-	int32_t current = prefix_funcs.at(index - 1);
-	while (current > 0) {
-		does_character_continue_string.at(result_arr.at(current)) = true;
-
-		current = prefix_funcs.at(current - 1);
-	}
-	does_character_continue_string.at(result_arr.at(current)) = true; // in case current == 0
+void FindMinStringCasePrefixFuncIsNull(const std::vector<uint32_t>& prefix_func
+														, std::vector<uint32_t>& result_arr
+														, uint32_t index) {
+	std::bitset<alphabet_size> does_character_continue_string;
+	int32_t current = prefix_func[index - 1];
+	for (; current > 0; current = prefix_func[current - 1])
+		does_character_continue_string.set(result_arr[current]);
+	does_character_continue_string.set(result_arr[current]); // in case current == 0
 
 	uint32_t i = 0;
-	for (const auto& val : does_character_continue_string) {
-		if (!val) {
+	for (size_t i = 0; i < does_character_continue_string.size(); ++i) {
+		if (!does_character_continue_string[i]) {
 			result_arr.push_back(i);
 			break;
 		}
-
 		++i;
 	}
 }
 
 std::string MininmalCorrespondingString(const std::vector<uint32_t>& prefix_funcs) {
-	const uint32_t alphabet_size = 26;
-
 	std::vector<uint32_t> result_arr(1, 0);
-
-	uint32_t index = 0;
-	for (const auto& prefix_func : prefix_funcs) {
-		if (!index) {
-			++index;
-			continue;
-		}
-
-		else if (prefix_func != 0)
-			result_arr.push_back(result_arr.at(prefix_func - 1));
-		
+	for (uint32_t i = 1; i < prefix_funcs.size(); ++i) {
+		if (prefix_funcs[i] != 0)
+			result_arr.push_back(result_arr[prefix_funcs[i] - 1]);
 		else
-			FindMinStringCasePrefixFuncIsNull(prefix_funcs, result_arr, index);
-
-		++index;
+			FindMinStringCasePrefixFuncIsNull(prefix_funcs, result_arr, i);
 	}
-	
+
 	std::string result_str;
 	for (const auto& i : result_arr)
 		result_str.push_back(i + 'a');
@@ -68,7 +51,6 @@ int main(int argc, char* argv[]) {
 			continue;
 		prefix_funcs.push_back(c - '0');
 	}
-
 	std::cout << MininmalCorrespondingString(prefix_funcs);
 
 	return 0;
