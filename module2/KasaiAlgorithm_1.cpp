50da3094f9f6a1698f19bf0df00ccc0685b1ea7d
and15121512 <andrew15121512@yandex.ru>
2019-11-30 22:13:01 +0300

    fixed

--- module2/KasaiAlgorithm_1.cpp
+++ module2/KasaiAlgorithm_1.cpp
@@ -34,30 +34,30 @@ void SuffixArray::Prepearing() {
 
 	std::vector<uint32_t> counts(std::max(k_alphabet_size, sz)); // count symbols number in string
 	for (const auto& ch : str)
-		++counts.at(ch);
+		++counts[ch];
 
 	// count position of beginnings of same value blocks
 	uint32_t carry = 0;
 	for (uint32_t i = 0; i < counts.size() - 1; ++i) {
-		uint32_t tmp = counts.at(i);
-		counts.at(i) = carry;
+		uint32_t tmp = counts[i];
+		counts[i] = carry;
 		carry += tmp;
 	}
 
 	uint32_t suff_beginning = 0;
 	for (const auto& ch : str) {
-		suffixes.at(counts.at(ch)) = suff_beginning;
-		++counts.at(ch);
+		suffixes[counts[ch]] = suff_beginning;
+		++counts[ch];
 		++suff_beginning;
 	}
 
 	uint32_t curr_eq_class = 0;
-	eq_classes.at(0) = 0;
+	eq_classes[0] = 0;
 	for (uint32_t i = 1; i < suffixes.size(); ++i) {
-		if (str.at(suffixes.at(i)) != str.at(suffixes.at(i - 1)))
+		if (str[suffixes[i]] != str[suffixes[i - 1]])
 			++curr_eq_class;
 
-		eq_classes.at(suffixes.at(i)) = curr_eq_class;
+		eq_classes[suffixes[i]] = curr_eq_class;
 	}
 }
 
@@ -73,45 +73,44 @@ SuffixArray::SuffixArray(const std::string& str) : str(str) {
 		std::vector<uint32_t> sorted_by_second_part(suffixes.size());
 
 		for (uint32_t i = 0; i < sorted_by_second_part.size(); ++i)
-			sorted_by_second_part.at(i) = CycledValue(suffixes.at(i) - curr_len);
+			sorted_by_second_part[i] = CycledValue(suffixes[i] - curr_len);
 
 		// next we can sort it using counting sort
 		std::vector<uint32_t> counts(suffixes.size());
 
 		for (const auto& suff : sorted_by_second_part)
-			++counts.at(eq_classes.at(suff));
+			++counts[eq_classes[suff]];
 
 		uint32_t carry = 0;
 		for (uint32_t i = 0; i < counts.size(); ++i) {
-			uint32_t tmp = counts.at(i);
-			counts.at(i) = carry;
+			uint32_t tmp = counts[i];
+			counts[i] = carry;
 			carry += tmp;
 		}
 
 		//uint32_t pref_beginning = 0;
 		for (const auto& suff : sorted_by_second_part) {
-			suffixes.at(counts.at(eq_classes.at(suff))) = suff;
-			++counts.at(eq_classes.at(suff));
-			//++pref_beginning;
+			suffixes[counts[eq_classes[suff]]] = suff;
+			++counts[eq_classes[suff]];
 		}
 
 		// change classes
 		std::vector<uint32_t> new_eq_classes(eq_classes.size());
 		uint32_t curr_eq_class = 0;
-		new_eq_classes.at(0) = 0;
+		new_eq_classes[0] = 0;
 		for (uint32_t i = 1; i < suffixes.size(); ++i) {
 			bool is_first_part_classes_equal =
-				eq_classes.at(suffixes.at(i)) == eq_classes.at(suffixes.at(i - 1));
+				eq_classes[suffixes[i]] == eq_classes[suffixes[i - 1]];
 			bool is_second_part_classes_equal =
-				eq_classes.at(CycledValue(suffixes.at(i) + curr_len))
-				== eq_classes.at(CycledValue(suffixes.at(i - 1) + curr_len));
+				eq_classes[CycledValue(suffixes[i] + curr_len)]
+				== eq_classes[CycledValue(suffixes[i - 1] + curr_len)];
 
 			if (!(is_first_part_classes_equal
 				&& is_second_part_classes_equal)) {
 				++curr_eq_class;
 			}
 
-			new_eq_classes.at(suffixes.at(i)) = curr_eq_class;
+			new_eq_classes[suffixes[i]] = curr_eq_class;
 		}
 
 		eq_classes = std::move(new_eq_classes);
@@ -125,35 +124,35 @@ std::vector<uint32_t> SuffixArray::lcp() {
 	std::vector<uint32_t> suff_positions(suffixes.size());
 
 	for (uint32_t i = 0; i < suff_positions.size(); ++i) {
-		suff_positions.at(suffixes.at(i)) = i;
+		suff_positions[suffixes[i]] = i;
 	}
 
 	// calculating lcp
 	std::vector<uint32_t> lcp(suffixes.size());
-	lcp.at(0) = 0; // first value undefined
+	lcp[0] = 0; // first value undefined
 	uint32_t prev_lcp = 0;
 
 	for (uint32_t i = 0; i < suff_positions.size(); ++i) {
 		// lcp for zero element in suffix array is undefined...
 		// ...so, we just skip it
-		if (suff_positions.at(i) == 0)
+		if (suff_positions[i] == 0)
 			continue;
 
 		// lcp for current = lcp for previous suffix in string - 1
 		// we have matching theorem
 		uint32_t curr_lcp = 0;
-		if (i > 0 && suff_positions.at(i - 1) != 0 && prev_lcp > 0) // if previous lcp exists (not lcp[0])
+		if (i > 0 && suff_positions[i - 1] != 0 && prev_lcp > 0) // if previous lcp exists (not lcp[0])
 			curr_lcp = prev_lcp - 1;
 
 		// find lcp
-		uint32_t j = suffixes.at(suff_positions.at(i) - 1); // previous suffix in suffix array
+		uint32_t j = suffixes[suff_positions[i] - 1]; // previous suffix in suffix array
 
 		for (; i + curr_lcp < str.size()
 			&& j + curr_lcp < str.size()
-			&& str.at(i + curr_lcp) == str.at(j + curr_lcp); ++curr_lcp);
-		lcp.at(suff_positions.at(i)) = curr_lcp;
+			&& str[i + curr_lcp] == str[j + curr_lcp]; ++curr_lcp);
+		lcp[suff_positions[i]] = curr_lcp;
 
-		prev_lcp = lcp.at(suff_positions.at(i));
+		prev_lcp = lcp[suff_positions[i]];
 	}
 
 	return lcp;
@@ -164,7 +163,7 @@ uint32_t SuffixArray::SubstringsCount() {
 	uint32_t result = 0;
 
 	for (uint32_t i = 1; i < str.size(); ++i) {
-		result += str.size() - suffixes.at(i) - lcp_arr.at(i) - 1;
+		result += str.size() - suffixes[i] - lcp_arr[i] - 1;
 	}
 
 	// we count 5 substrings with $
