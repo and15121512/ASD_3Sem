78ec7cecc6fe80ba36bece2dcc8a85d4c5c2f1ea
and15121512 <andrew15121512@yandex.ru>
2019-12-13 15:03:34 +0300

    fixed

--- module1/Aho_Korasik_Trie_3.cpp
+++ module1/Aho_Korasik_Trie_3.cpp
@@ -7,250 +7,252 @@
 
 
 
+inline char index_from_char(char c) { return c - 'a'; }
+
 class Trie {
 public:
-	Trie(const std::vector<std::vector<char>>& strings);
+    Trie(const std::vector<std::vector<char>>& strings);
 
-	static void PrintAllPatternOccurences(const std::vector<char>& mask);
+    static void PrintAllPatternOccurences(const std::vector<char>& mask);
 
 private:
-	struct Node;
-	typedef std::shared_ptr<Node> shared_ptr;
+    struct Node;
+    //typedef std::unique_ptr<Node> unique_ptr;
 
-	shared_ptr root;
-	std::vector<std::vector<char>> strings;
+    std::unique_ptr<Node> root;
+    std::vector<std::vector<char>> strings;
 
-	shared_ptr current_state; // текущее состояние бора, соответствующее переданной
-							// в него ранее последовательности
+    Node* current_state;
 
-	shared_ptr SuffixLink(shared_ptr node);
-	shared_ptr CompressedSuffixLink(shared_ptr node);
-	shared_ptr NextForTransition(shared_ptr node, char c);
+    Node* SuffixLink(Node* node);
+    Node* CompressedSuffixLink(Node* node);
+    Node* NextForTransition(Node* node, char c);
 
-	void AddString(uint32_t string_index);
+    void AddString(uint32_t string_index);
 
-	bool IsRoot(const shared_ptr node) const { return node == root; }
+    bool IsRoot(const Node* node) const { return node == root.get(); }
 
-	static void PrintMasklessPartsOccurences(const std::vector<std::vector<char>>& strings
-		, const std::vector<uint32_t>& strings_indexes
-		, uint32_t mask_size);
+    static void PrintMasklessPartsOccurences(const std::vector<std::vector<char>>& strings
+        , const std::vector<uint32_t>& strings_indexes
+        , uint32_t mask_size);
 
-	static void PrintPatternOccurences(const std::vector<uint32_t>& numbers_of_corresp_strings
-		, uint32_t text_size
-		, uint32_t mask_size
-		, uint32_t patterns_without_mask_number);
+    static void PrintPatternOccurences(const std::vector<uint32_t>& numbers_of_corresp_strings
+        , uint32_t text_size
+        , uint32_t mask_size
+        , uint32_t patterns_without_mask_number);
 };
 
+const uint32_t k_alphabet_size = 26;
+
 struct Trie::Node {
-	std::vector<shared_ptr> sons;
-	bool is_leaf; // по умолчанию все node НЕ is_leaf
-	std::vector<uint32_t> corresp_string; // индексы соответствующих строк
-	std::vector<shared_ptr> next_for_transition;
+    std::vector<std::unique_ptr<Node>> sons;
+    bool is_leaf;
+    std::vector<uint32_t> corresp_string;
+    std::vector<Node*> next_for_transition;
 
-	shared_ptr parent;
-	shared_ptr suff_link;
-	shared_ptr up_link;
-	int32_t parent_edge_char;
+    Node* parent;
+    Node* suff_link;
+    Node* up_link;
+    int32_t parent_edge_char;
 
-	Node(shared_ptr parent
-		, int32_t parent_edge_char = INT32_MAX) : parent(parent)
-		, corresp_string(std::vector<uint32_t>())
-		, suff_link(nullptr)
-		, up_link(nullptr)
-		, parent_edge_char(parent_edge_char)
-		, is_leaf(false) {
-		const uint32_t k_alphabet_size = 26;
-		sons = std::vector<shared_ptr>(k_alphabet_size, shared_ptr(nullptr));
-		next_for_transition = std::vector<shared_ptr>(k_alphabet_size, shared_ptr(nullptr));
-	}
+    Node(Node* parent
+        , int32_t parent_edge_char = INT32_MAX) : parent(parent)
+        , corresp_string(std::vector<uint32_t>())
+        , suff_link(nullptr)
+        , up_link(nullptr)
+        , parent_edge_char(parent_edge_char)
+        , is_leaf(false) {
+        for (size_t i = 0; i < k_alphabet_size; ++i)
+            sons.emplace_back(nullptr);
+        next_for_transition = std::vector<Node*>(k_alphabet_size, nullptr);
+    }
 
-	shared_ptr Son(char c) const { return sons[c - 'a']; }
-	shared_ptr NextForTransition(char c) const { return next_for_transition[c]; }
+    Node* Son(char c) const { return sons[index_from_char(c)].get(); }
+    Node* NextForTransition(char c) const { return next_for_transition[c]; }
 
-	void AddSon(char c, shared_ptr son) { sons[c - 'a'] = son; }
-	void AddCorrespString(uint32_t string) { corresp_string.push_back(string); }
-	void AddCorrespStrings(std::vector<uint32_t>& strings) {
-		for (const auto& str : corresp_string)
-			strings.push_back(str);
-	}
+    void MakeSon(char c, Node* node) {
+        Node* son = new Node(node, c);
+        sons[index_from_char(c)].reset(son);
+    }
+    void AddCorrespString(uint32_t string) { corresp_string.push_back(string); }
+    void AddCorrespStrings(std::vector<uint32_t>& strings) {
+        for (const auto& str : corresp_string)
+            strings.push_back(str);
+    }
 };
 
 Trie::Trie(const std::vector<std::vector<char>>& strings_) {
-	strings = strings_;
-	//root = new Node(nullptr);
-	root = std::make_shared<Node>(nullptr);
-	current_state = root;
+    strings = strings_;
+    root = std::make_unique<Node>(nullptr);
+    current_state = root.get();
 
-	for (uint32_t strs_index = 0; strs_index < strings.size(); ++strs_index)
-		AddString(strs_index);
+    for (uint32_t strs_index = 0; strs_index < strings.size(); ++strs_index)
+        AddString(strs_index);
 }
 
 void Trie::AddString(uint32_t string_index) {
-	shared_ptr current = root;
-	for (const auto& c : strings[string_index]) {
-		if (!current->Son(c)) {
-			shared_ptr son = std::make_shared<Node>(current, c);
-			current->AddSon(c, son);
-		}
+    Node* current = root.get();
+    for (const auto& c : strings[string_index]) {
+        if (!current->Son(c)) {
+            current->MakeSon(c, current);
+        }
 
-		current = current->Son(c);
-	}
+        current = current->Son(c);
+    }
 
-	current->AddCorrespString(string_index);
-	current->is_leaf = true;
+    current->AddCorrespString(string_index);
+    current->is_leaf = true;
 }
 
-Trie::shared_ptr Trie::SuffixLink(shared_ptr node) {
-	if (!node->suff_link) {
+Trie::Node* Trie::SuffixLink(Node* node) {
+    if (!node->suff_link) {
 
-		if (node == root || node->parent == root)
-			node->suff_link = root;
+        if (node == root.get() || node->parent == root.get())
+            node->suff_link = root.get();
 
-		else
-			node->suff_link = NextForTransition(SuffixLink(node->parent), node->parent_edge_char);
-	}
+        else
+            node->suff_link = NextForTransition(SuffixLink(node->parent), node->parent_edge_char);
+    }
 
-	return node->suff_link;
+    return node->suff_link;
 }
 
-Trie::shared_ptr Trie::NextForTransition(shared_ptr node, char c) {
-	char c_index = c - 'a';
+Trie::Node* Trie::NextForTransition(Node* node, char c) {
+    char c_index = index_from_char(c);
 
-	if (!node->next_for_transition[c_index]) {
+    if (!node->next_for_transition[c_index]) {
 
-		if (node->Son(c))
-			node->next_for_transition[c_index] = node->Son(c);
+        if (node->Son(c))
+            node->next_for_transition[c_index] = node->Son(c);
 
-		else if (node == root)
-			node->next_for_transition[c_index] = root;
+        else if (node == root.get())
+            node->next_for_transition[c_index] = root.get();
 
-		else
-			node->next_for_transition[c_index] = NextForTransition(SuffixLink(node), c);
-	}
+        else
+            node->next_for_transition[c_index] = NextForTransition(SuffixLink(node), c);
+    }
 
-	return node->next_for_transition[c_index];
+    return node->next_for_transition[c_index];
 }
 
-Trie::shared_ptr Trie::CompressedSuffixLink(shared_ptr node) {
+Trie::Node* Trie::CompressedSuffixLink(Node* node) {
 
-	if (!node->up_link) {
-		shared_ptr suff_link = SuffixLink(node);
+    if (!node->up_link) {
+        Node* suff_link = SuffixLink(node);
 
-		if (suff_link->is_leaf)
-			node->up_link = suff_link;
+        if (suff_link->is_leaf)
+            node->up_link = suff_link;
 
-		else if (SuffixLink(node) == root)
-			node->up_link = root;
+        else if (SuffixLink(node) == root.get())
+            node->up_link = root.get();
 
-		else
-			node->up_link = CompressedSuffixLink(suff_link);
-	}
-	return node->up_link;
+        else
+            node->up_link = CompressedSuffixLink(suff_link);
+    }
+    return node->up_link;
 }
 
 void Trie::PrintPatternOccurences(const std::vector<uint32_t>& numbers_of_corresp_strings
-	, uint32_t text_size
-	, uint32_t mask_size
-	, uint32_t patterns_without_mask_number) {
-	uint32_t index = 0;
-	for (const uint32_t& num : numbers_of_corresp_strings) {
-		if (index + mask_size > text_size)
-			break;
-		if (num == patterns_without_mask_number)
-			std::cout << index << ' ';
-		++index;
-	}
+    , uint32_t text_size
+    , uint32_t mask_size
+    , uint32_t patterns_without_mask_number) {
+    uint32_t index = 0;
+    for (const uint32_t& num : numbers_of_corresp_strings) {
+        if (index + mask_size > text_size)
+            break;
+        if (num == patterns_without_mask_number)
+            std::cout << index << ' ';
+        ++index;
+    }
 }
 
 void Trie::PrintMasklessPartsOccurences(const std::vector<std::vector<char>>& strings
-	, const std::vector<uint32_t>& strings_indexes
-	, uint32_t mask_size) {
-	Trie trie(strings);
-	std::vector<uint32_t> numbers_of_corresp_strings;
-	// число безмасочных кусков шаблона, которые стоят на своей позиции в шаблоне, если бы шаблон
-	// начинался в данном индексе
+    , const std::vector<uint32_t>& strings_indexes
+    , uint32_t mask_size) {
+    Trie trie(strings);
+    std::vector<uint32_t> numbers_of_corresp_strings;
 
-	const char k_terminate_symbol = '|';
-	uint32_t index = 0;
-	for (; ; ++index) {
-		char c;
-		std::cin >> c;
-		if (std::cin.eof() || c == k_terminate_symbol)
-			break;
+    const char k_terminate_symbol = '|';
+    uint32_t index = 0;
+    for (; ; ++index) {
+        char c;
+        std::cin >> c;
+        if (std::cin.eof() || c == k_terminate_symbol)
+            break;
 
-		numbers_of_corresp_strings.push_back(0);
+        numbers_of_corresp_strings.push_back(0);
 
-		trie.current_state = trie.NextForTransition(trie.current_state, c);
+        trie.current_state = trie.NextForTransition(trie.current_state, c);
 
-		if (!(trie.CompressedSuffixLink(trie.current_state) == trie.root)) {
-			shared_ptr current = trie.current_state;
+        if (!(trie.CompressedSuffixLink(trie.current_state) == trie.root.get())) {
+            Node* current = trie.current_state;
 
-			while (!trie.IsRoot(current)) {
-				current = trie.CompressedSuffixLink(current);
+            while (!trie.IsRoot(current)) {
+                current = trie.CompressedSuffixLink(current);
 
-				for (const uint32_t& occur_index : current->corresp_string) {
-					const int32_t begin_index = index + 1 - strings_indexes[occur_index];
-					if (begin_index >= 0) // если шаблон не вылезет за текст
-						++numbers_of_corresp_strings[begin_index];
-				}
-			}
-		}
+                for (const uint32_t& occur_index : current->corresp_string) {
+                    const int32_t begin_index = index + 1 - strings_indexes[occur_index];
+                    if (begin_index >= 0)
+                        ++numbers_of_corresp_strings[begin_index];
+                }
+            }
+        }
 
-		if (trie.current_state->is_leaf) {
-			for (const uint32_t& occur_index : trie.current_state->corresp_string) {
-				const int32_t begin_index = index + 1 - strings_indexes[occur_index];
-				if (begin_index >= 0) // если шаблон не вылезет за текст
-					++numbers_of_corresp_strings[begin_index];
-			}
-		}
+        if (trie.current_state->is_leaf) {
+            for (const uint32_t& occur_index : trie.current_state->corresp_string) {
+                const int32_t begin_index = index + 1 - strings_indexes[occur_index];
+                if (begin_index >= 0)
+                    ++numbers_of_corresp_strings[begin_index];
+            }
+        }
 
-	}
+    }
 
-	PrintPatternOccurences(numbers_of_corresp_strings, index, mask_size, strings.size());
+    PrintPatternOccurences(numbers_of_corresp_strings, index, mask_size, strings.size());
 }
 
 void Trie::PrintAllPatternOccurences(const std::vector<char>& mask) {
-	const char k_mask_delimiter = '?';
+    const char k_mask_delimiter = '?';
 
-	std::vector<std::vector<char>> strings(1, std::vector<char>());
-	std::vector<uint32_t> strings_indexes;
+    std::vector<std::vector<char>> strings(1, std::vector<char>());
+    std::vector<uint32_t> strings_indexes;
 
-	uint32_t index = 0;
-	for (const char& c : mask) {
-		if (c == k_mask_delimiter) {
-			if (!strings.back().empty()) {
-				strings_indexes.push_back(index);
-				strings.push_back(std::vector<char>());
-			}
-		}
-		else
-			strings.back().push_back(c);
+    uint32_t index = 0;
+    for (const char& c : mask) {
+        if (c == k_mask_delimiter) {
+            if (!strings.back().empty()) {
+                strings_indexes.push_back(index);
+                strings.push_back(std::vector<char>());
+            }
+        }
+        else
+            strings.back().push_back(c);
 
-		++index;
-	}
+        ++index;
+    }
 
-	if (strings.back().empty())
-		strings.pop_back();
-	else
-		strings_indexes.push_back(index);
+    if (strings.back().empty())
+        strings.pop_back();
+    else
+        strings_indexes.push_back(index);
 
-	PrintMasklessPartsOccurences(strings, strings_indexes, mask.size());
+    PrintMasklessPartsOccurences(strings, strings_indexes, mask.size());
 }
 
 //-------------------------------------------------------------------------------------
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-	std::vector<char> mask;
-	std::string mask_str;
+    std::vector<char> mask;
+    std::string mask_str;
 
-	std::cin >> mask_str;
-	for (const auto& symb : mask_str)
-		mask.push_back(symb);
+    std::cin >> mask_str;
+    for (const auto& symb : mask_str)
+        mask.push_back(symb);
 
-	Trie::PrintAllPatternOccurences(mask);
+    Trie::PrintAllPatternOccurences(mask);
 
-	return 0;
+    return 0;
 }
